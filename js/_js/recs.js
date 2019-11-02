ec67ff657e9f53adcdcf038b56ca2eed76565f82
pavelp21 <pavelvp2182@yandex.ru>
2019-11-02 16:49:59 +0500

    Records page is typeseted

---
+++ js/_js/recs.js
@@ -0,0 +1,79 @@
+//@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
+//@prepros-prepend owl.carousel.min.js
+
+var isIE = false || !!document.documentMode;
+
+if (isIE) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "css/course-ie.min.css";
+    head.appendChild(link);
+}
+
+$(document).ready(function () {
+    /* Якорь */
+    $(function () {
+        $("a[href^='#prices']").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    });
+
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            buttons: [
+                'slideShow',
+                'share',
+                'zoom',
+                'fullScreen',
+                // 'download',
+                'close'
+            ],
+            speed: 330,
+            loop: true,
+            opacity: "auto",
+            // autoScale: true,
+            mouseWheel: true,
+            transitionEffect: 'slide'
+        });
+    });
+    /* Записи */
+    (function ($) {
+        jQuery.fn.lightTabs = function (options) {
+
+            var createTabs = function () {
+                tabs = this;
+                i = 0;
+
+                showPage = function (i) {
+                    $(tabs).children("div").children("div").hide();
+                    $(tabs).children("div").children("div").eq(i).show();
+                    $(tabs).children("ul").children("li").removeClass("active");
+                    $(tabs).children("ul").children("li").eq(i).addClass("active");
+                }
+
+                showPage(0);
+
+                $(tabs).children("ul").children("li").each(function (index, element) {
+                    $(element).attr("data-page", i);
+                    i++;
+                });
+
+                $(tabs).children("ul").children("li").click(function () {
+                    showPage(parseInt($(this).attr("data-page")));
+                });
+            };
+            return this.each(createTabs);
+        };
+    })(jQuery);
+
+    $("#recs").lightTabs();
+    /*Конец документа*/
+});
